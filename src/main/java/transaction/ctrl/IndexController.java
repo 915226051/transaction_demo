2a1ba9c25ebf2c676a2e2027ea8933150cc9af95
915226051@qq.com <Lyq1997118>
2019-08-21 00:43:09 +0800

    SqlSessionFactory扫描至多个Mapper.xml文件 而自动扫描的映射文件为DAO的包路径。负责扫描不到。

--- src/main/java/transaction/ctrl/IndexController.java
+++ src/main/java/transaction/ctrl/IndexController.java
@@ -1,13 +1,22 @@
 package transaction.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import transaction.service.BookService;
+import transaction.vo.Book;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/ctrl")
 public class IndexController {
+
+    @Autowired
+    BookService bookService;
+
     @RequestMapping("/index")
     @ResponseBody
     public String index() {
@@ -18,5 +27,19 @@ public class IndexController {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("index");
         return mv;
+    }
+    @RequestMapping("/add")
+    public String add(){
+        Book book = new Book();
+        book.setId(1);
+        book.setPrice(3.1415);
+        book.setPublishDate(new Date());
+        book.setTitle("啥啊");
+        try {
+            bookService.add(book);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "success";
     }
 }
